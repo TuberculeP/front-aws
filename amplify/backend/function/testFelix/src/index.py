fce555dee79f2c8c1c724cf00896dd7e1e14553d
Félix Laviéville <lavieville.felix@gmail.com>
2025-07-21 14:42:02 +0200

    feat: pushing function$

---
+++ amplify/backend/function/testFelix/src/index.py
@@ -0,0 +1,18 @@
+import json
+
+def function_to_test(a, b):
+  return a + b
+
+def handler(event, context):
+  print('received event:')
+  print(event)
+  
+  return {
+      'statusCode': 200,
+      'headers': {
+          'Access-Control-Allow-Headers': '*',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+      },
+      'body': json.dumps('Hello from your new Amplify Python lambda!')
+  }
---
+++ amplify/backend/test_functions.py
@@ -0,0 +1,4 @@
+from function.testFelix.src.index import function_to_test
+
+def test_answer():
+    assert function_to_test(3, 2) == 5
